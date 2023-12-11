e7969c2628d4946b5beccb81eda21ab97d1135a7
unknown <motifrempong@gmail.com>
2023-12-11 17:52:50 +0000

    updated my dashboard

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import Navbar from './components/inc/Navbar';
-import LandingPage from './components/pages/LandingPage';
-import Login from './components/pages/Login';
-import Dashboard from './components/pages/Dashboard';
-import Contact from './components/pages/Contact';
+import Navbar from './components/Navbar';
+import LandingPage from './components/LandingPage';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+import GradeReport from './components/Grade Report';
+import Faqs from './components/faqs';
+import Missinggradeform from './components/Missing grade form';
+import Contact from './components/Contact';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+
 
 function App() {  
   return (
@@ -17,7 +23,10 @@ function App() {
   <Route path="/LandingPage" element={<LandingPage />} />
   <Route path="/Login" element={<Login/>} />
   <Route path="/Dashboard" element={<Dashboard/>} />
+  <Route path="/Grade Report" element={<GradeReport/>} />
+  <Route path="/Missing grade form" element={<Missinggradeform/>} />
   <Route path="/Contact" element={<Contact/>} />
+  <Route path="/faqs" element={<Faqs/>} />
 </Routes>
 
     </div>
---
+++ my-react-app/src/components/Grade Report.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function  GradeReport(){
+  return(
+    <div>
+      <div className="container">
+        <div className="card mt-4">
+        <div className="card-body">
+          <h2> GradeReport</h2>
+        </div>
+      </div>
+    </div>
+    </div>
+  );
+}
+
+export default GradeReport;
